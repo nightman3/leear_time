a3885a032f5effeb3077ce8681781be58d041310
night-man <evmasa@yandex.ru>
2022-12-08 19:15:27 +0300

    Start learning git

---
+++ test.py
@@ -0,0 +1,16 @@
+import datetime
+now = datetime.datetime.now()
+hour = now.hour
+
+
+if hour > 0 and hour <=6:
+    print("Доброе ночи!")
+
+elif hour > 6 and hour <=12:
+    print("Доброе утро!")
+
+elif hour > 12 and hour <=18:
+    print("Добрый день!")
+
+elif hour >18 and  hour <=24:
+    print("Добрый вечер!")
